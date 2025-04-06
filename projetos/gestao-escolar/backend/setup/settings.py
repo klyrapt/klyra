82e491ce34e7801af7e9f3f7bfa43e15820b5d26
klyrapt <klyra.pt@gmail.com>
2025-04-06 23:55:45 +0100

    fix: coreçao de exibir foto de perfil

--- projetos/gestao-escolar/backend/setup/settings.py
+++ projetos/gestao-escolar/backend/setup/settings.py
@@ -171,9 +171,9 @@ STATIC_URL = 'static/'
 
 
 
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
--- projetos/gestao-escolar/backend/setup/urls.py
+++ projetos/gestao-escolar/backend/setup/urls.py
@@ -37,6 +37,5 @@ urlpatterns = [
     path("api/auth/", include("auntenticacao.urls")),
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
