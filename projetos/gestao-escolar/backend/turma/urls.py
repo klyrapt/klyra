bd39eb5f603dd1ae38c893ca271f86c108cd9a40
klyrapt <klyra.pt@gmail.com>
2025-04-07 22:05:42 +0100

    feat adicionando DelatePopup ao formulario de cadastro da turma

--- projetos/gestao-escolar/backend/turma/urls.py
+++ projetos/gestao-escolar/backend/turma/urls.py
@@ -1,9 +1,7 @@
-# turma/urls.py
-
 from django.urls import path
-from .views import TurmaListCreateAPIView
+from .views import TurmaListCreateAPIView, TurmaDetailAPIView
 
 urlpatterns = [
     path("turmas/", TurmaListCreateAPIView.as_view(), name="turma-list-create"),
-    
+    path("turmas/<int:pk>/", TurmaDetailAPIView.as_view(), name="turma-detail"),
 ]
